9dd4bb5b79d33e20c6deed2d023f27d202abc24a
mehran <mehranmohamadi1311@gmail.com>
2025-09-10 14:59:04 +0330

    feat(footer): add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import { useImages } from "@images/useImages";
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 px-4 left-0 right-0  bg-navy-light flex justify-between items-center">
+      <img src={useImages.home} alt="homeIcon" className=" cursor-pointer" />
+      <img
+        src={useImages.calendar}
+        alt="calendarIcon"
+        className=" cursor-pointer "
+      />
+      <img
+        src={useImages.waterlevel}
+        alt=""
+        className="transform -translate-y-2 translate-x-2  cursor-pointer"
+      />
+      <img src={useImages.radar} alt="radarIcon" className="  cursor-pointer" />
+      <img
+        src={useImages.emergency}
+        alt="emergencyIcon"
+        className=" transform -translate-y-0.5 cursor-pointer"
+      />
+    </footer>
+  );
+};
