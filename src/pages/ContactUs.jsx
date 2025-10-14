128e0ef1d32918b34e65048d3ff57d1db0aaeb61
mehran <mehranmohamadi1311@gmail.com>
2025-10-14 14:20:53 +0330

    feat: connect form contact to my gmail (closes #6)

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,21 +1,71 @@
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 // Images;
 import { useImages } from "@images/useImages";
 
+const ACCESS_KEY = "03090a62-0231-4ad5-b4bc-bff9235fa448";
+const URL = "https://api.web3forms.com/submit";
 
 export const ContactUs = () => {
-
   const navigate = useNavigate();
+  // const [formResponse, setFormResponse] = useState([]);
+
+  const [formResponse, setFormResponse] = useState({ message: "", type: "" });
+
+  const handleSubmitForm = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    formData.append("access_key", ACCESS_KEY);
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    try {
+      const res = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+
+      const data = await res.json();
+
+      if (data.success) {
+        setFormResponse({ message: data.message, type: "success" });
+        event.target.reset();
+        console.log(data);
+      } else {
+        setFormResponse({
+          message: data.message || "Something went wrong!",
+          type: "error",
+        });
+        console.log(data);
+      }
+
+      setTimeout(() => {
+        setFormResponse({ message: "", type: "" });
+      }, 3000);
+    } catch (error) {
+      console.error("Error:", error);
+      setFormResponse({ message: "Network error!", type: "error" });
+      setTimeout(() => {
+        setFormResponse({ message: "", type: "" });
+      }, 3000);
+    }
+  };
 
   return (
     <section className="bg-navy flex h-dvh flex-col overflow-y-hidden">
       <header className="bg-navy-dark flex items-center space-x-12 py-6 pl-8">
-       <div className="cursor-pointer" onClick={() => navigate(-1)}>
-        <img
-          src={useImages.arrow}
-          alt="arrowIcon"
-          className="max-h-[28px] max-w-[28px]"
+        <div className="cursor-pointer" onClick={() => navigate(-1)}>
+          <img
+            src={useImages.arrow}
+            alt="arrowIcon"
+            className="max-h-[28px] max-w-[28px]"
           />
         </div>
         <h1 className="text-lightBlue text-3xl font-bold tracking-wider">
@@ -57,24 +107,40 @@ export const ContactUs = () => {
         </article>
       </main>
       <footer className="mt-2 flex flex-1 flex-col px-4">
-        <form className="bg-lightBlue/70 flex flex-1 flex-col gap-3 rounded-t-2xl px-4 pt-3 pb-2">
+        <form
+          onSubmit={handleSubmitForm}
+          className="bg-lightBlue/70 relative flex flex-1 flex-col gap-3 rounded-t-2xl px-4 pt-3 pb-2"
+        >
+          {formResponse.message && (
+            <p
+              className={`ease absolute right-7 ${formResponse.message ? "bottom-4" : "-bottom-full"} ${formResponse.type === "success" ? "text-green-700" : "text-red-600"} font-semibold transition-all duration-400`}
+            >
+              {formResponse.message}
+            </p>
+          )}
+
           <h3 className="text-navy text-center text-[23px] font-bold">
             Send a message
           </h3>
           <input
+            name="name"
             type="text"
             placeholder="Your name"
             className="bg-lightBlue placeholder:text-red-black/50 ring-navy transition-ring text-navy rounded-[10px] border-0 px-4 py-3 outline-0 duration-200 placeholder:text-[18px] placeholder:font-normal focus:ring-2"
           />
           <input
+            name="email"
             type="text"
             placeholder="Your Email"
             className="bg-lightBlue ring-navy transition-ring text-navy rounded-[10px] border-0 px-4 py-3 outline-0 duration-200 placeholder:text-[18px] placeholder:font-normal placeholder:text-black/50 focus:ring-2"
+            required
           />
           <textarea
+            name="message"
             placeholder="Write your message here"
             className="bg-lightBlue ring-navy transition-ring text-navy flex-1 resize-none rounded-[10px] border-0 px-4 py-4 outline-0 duration-200 placeholder:text-[18px] placeholder:font-normal placeholder:text-black/50 focus:ring-2"
           ></textarea>
+          <input type="submit" className="hidden" />
         </form>
       </footer>
     </section>
